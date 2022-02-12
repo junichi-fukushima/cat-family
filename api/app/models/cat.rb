c33be3cc7f741816c3ef5267104d4ea249f0a8ca
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2022-02-12 13:27:48 +0900

    activestorageの追加

--- api/app/models/cat.rb
+++ api/app/models/cat.rb
@@ -2,4 +2,10 @@ class Cat < ApplicationRecord
   # ActiveHash
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :cat_label
+
+  # ActiveStorage(picture)
+  has_one_attached :main_img
+  has_many_attached :sub_img
+
+
 end
