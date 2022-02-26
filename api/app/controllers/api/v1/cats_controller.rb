14b65e1296bea19b2ad3da0fae84051455fa2fe1
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2022-02-26 16:42:16 +0900

    メイン画像とサブ画像をそれぞれ返却するメソッドを用意する

--- api/app/controllers/api/v1/cats_controller.rb
+++ api/app/controllers/api/v1/cats_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class CatsController < ApplicationController
       def index
-        render json: Cat.all, methods: [:image_url]
+        render json: Cat.all, methods: [:main_image_url,:sub_image_url]
       end
     end
   end
