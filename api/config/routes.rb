e922d0c90993305ee3468a512886b1545d09326c
junichi-fukushima <71912259+junichi-fukushima@users.noreply.github.com>
2022-03-05 15:47:53 +0900

    deviceの導入

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
       get "cat_label", to: "cat_label#index"
       get "cat_sex", to: "cat_sex#index"
       get "cat_type", to: "cat_type#index"
+      # ユーザー認証
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 end
